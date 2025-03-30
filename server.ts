a47018dad054917c69ce5aec50a104f77cf787ee
Cowbilet <sebgtaylor@gmail.com>
2025-03-30 14:04:41 +1100

    added server.ts to serve the files

---
+++ server.ts
@@ -0,0 +1,15 @@
+import express from 'express'
+const app = express();
+
+// import payment from "./shop.js"
+app.use(express.json());
+
+
+// app.use("/api/store", payment);
+
+app.use("/", express.static("./dist"))
+
+// Serve the files on port 3000.
+app.listen(3000, function () {
+    console.log('Robox website listening on port 3000!\n');
+}); 
