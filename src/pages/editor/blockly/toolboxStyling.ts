db0b6365683fb1295416377930955957e908852e
Cowbilet <sebgtaylor@gmail.com>
2025-04-05 22:20:14 +1100

    added extending tabs

--- src/pages/editor/blockly/toolboxStyling.ts
+++ src/pages/editor/blockly/toolboxStyling.ts
@@ -2,6 +2,13 @@ import * as Blockly from 'blockly';
 
 import archSVG from "./Arch.svg"
 type HexColor = `#${string}`;
+
+const animationTime = 300
+const extensionAmount = 20;
+
+const archWidth = 48
+const iconHeight = 20
+
 class RoboxToolboxCategories extends Blockly.ToolboxCategory {
     /**
      * Constructor for a custom category.
@@ -17,13 +24,16 @@ class RoboxToolboxCategories extends Blockly.ToolboxCategory {
     /** @override */
     createRowContentsContainer_() {
         const dom = super.createRowContentsContainer_();
+        const rectangle = document.createElement("div")
+        rectangle.className = "extender"
+        rectangle.style.background = this.colour_
         const container = document.createElement("div")
         container.className = "side-arch"
         container.innerHTML = archSVG
         const arch = container.querySelector("svg > path")
         if (!arch || !(arch instanceof SVGElement)) return dom
         arch.style.fill = this.colour_;
-        
+        dom.appendChild(rectangle)
         dom.appendChild(container)
         return dom
     }
@@ -36,6 +46,40 @@ class RoboxToolboxCategories extends Blockly.ToolboxCategory {
     }
     /** @override */
     setSelected(isSelected: boolean){
+        const dom = this.htmlDiv_
+        const icon = dom?.querySelector(".categoryIcon")
+        const extender = dom?.querySelector(".extender")
+        if (!icon || !(icon instanceof HTMLElement)) return
+        if (!extender || !(extender instanceof HTMLElement)) return
+        if (isSelected) {
+            const extenderAnimation = [
+                { width: `${extender.offsetWidth}px` },
+                { width: `${extensionAmount}px` },
+            ];
+            const iconAnimation = [
+                { left: `${icon.offsetLeft}px` },
+                { left: `${((archWidth/3)-(iconHeight/2))+extensionAmount}px` },
+            ];
+            icon.animate(iconAnimation, { duration: animationTime, iterations: 1})
+            extender.animate(extenderAnimation, { duration: animationTime, iterations: 1})
+            extender.classList.add("extended")
+            icon.classList.add("extended")
+        }
+        else {
+            const extenderAnimation = [
+                { width: `${extender.offsetWidth}px` },
+                { width: `0px` },
+            ];
+            const iconAnimation = [
+                { left: `${icon.offsetLeft}px` },
+                { left: `${((archWidth/3)-(iconHeight/2))}px` },
+            ];
+
+            icon.animate(iconAnimation, { duration: animationTime, iterations: 1})
+            extender.animate(extenderAnimation, { duration: animationTime, iterations: 1})
+            extender.classList.remove("extended")
+            icon.classList.remove("extended")
+        }
     }
 }
 class RoboxToolboxSeperator extends Blockly.ToolboxSeparator {
