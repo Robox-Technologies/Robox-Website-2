a4e642a7ac2717d46e9a06952a044247fc849f1a
Yuma Soerianto <hi@madebyyuma.com>
2025-05-01 16:31:20 +1000

    Updated hero and made webpack paths absolute

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,1 +1,12 @@
 import './boxToBot.ts';
+
+const scrollPrompt = document.querySelector(".scroll-prompt") as HTMLElement;
+const scrollTarget = document.getElementById("scrollTarget") as HTMLElement;
+
+scrollPrompt.onclick = (e) => {
+    e.stopPropagation();
+
+    scrollTarget.scrollIntoView({
+        behavior: 'smooth'
+    });
+}
--- webpack.config.js
+++ webpack.config.js
@@ -84,7 +84,8 @@ const config = {
     },
     output: {
         clean: true,
-        devtoolModuleFilenameTemplate: '[absolute-resource-path]'
+        devtoolModuleFilenameTemplate: '[absolute-resource-path]',
+        publicPath: '/'
     }
 };
 function getHtmlFiles(directory, rootDir) {
