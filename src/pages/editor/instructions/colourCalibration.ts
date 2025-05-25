352281400346f0acfac2c1a61365be82e278aa60
Cowbilet <sebgtaylor@gmail.com>
2025-05-25 23:34:35 +1000

    added full logic for the colour calibration

--- src/pages/editor/instructions/colourCalibration.ts
+++ src/pages/editor/instructions/colourCalibration.ts
@@ -1,31 +1,105 @@
 import { pico } from "../communication/communicate"
 let calibrated = false
+let waitingForCalibration = false
+//TODO: Probably need to do some cleanup for this
+
 document.addEventListener("DOMContentLoaded", () => {
+    // Define the calibration model stage 1
     let calibrateModal = document.getElementById("color-calibration-modal") as HTMLDialogElement | null
     if (!calibrateModal) return
-    let calibrateOutcomeModal = document.getElementById("calibrate-outcome-button") as HTMLDialogElement | null
+    let calibrateOutcomeModal = document.getElementById("color-calibration-outcome-modal") as HTMLDialogElement | null
     if (!calibrateOutcomeModal) return
     calibrateModal.showModal()
     let calibrateButton = document.getElementById("calibrate-button") as HTMLButtonElement | null
     if (!calibrateButton) return
-    calibrateButton.addEventListener("click", calibrate)
-
+    // Define the calibration model stage 2
+    let outcomeText = calibrateOutcomeModal.querySelector(".modal-content p") as HTMLParagraphElement | null
+    if (!outcomeText) return
+    let outcomeButton = calibrateOutcomeModal.querySelector("#calibrate-outcome-button") as HTMLButtonElement | null
+    if (!outcomeButton) return
+    let outcomeTitle = calibrateOutcomeModal.querySelector(".modal-title") as HTMLHeadingElement | null
+    if (!outcomeTitle) return
+    
+    // When the pico tells it is calibrated
     pico.addEventListener("calibrated", () => {
         calibrated = true //Successful calibration!
-        calibrateModal.close()
-        let outcomeText = calibrateOutcomeModal.querySelector(".modal-content p") as HTMLParagraphElement | null
-        if (!outcomeText) return
-        let outcomeButton = calibrateOutcomeModal.querySelector("#calibrate-outcome-button") as HTMLButtonElement | null
-        if (!outcomeButton) return
+
         outcomeText.textContent = "Calibration was a success!\nThe calibration settings have been saved to the Ro/Box's Pico."
         outcomeButton.textContent = "Done"
+        outcomeTitle.textContent = "Ro/Box Calibrated!"
+        // Close whatever modal is already open
+        if (calibrateModal.hasAttribute("open")) calibrateModal.close()
+        if (calibrateOutcomeModal.hasAttribute("open")) calibrateOutcomeModal.close()
+        calibrateOutcomeModal.showModal()
     })
-})
-function calibrate() {
-    pico.calibrate()
-    setTimeout(() => {
-        if (!calibrated) { // Failed for calibrate
+    
+    // When the pico errors
+    pico.addEventListener("error", (event) => {
+        const picoEvent = event as CustomEvent
+        // When the pico says it isnt connected to a colour sensor
+        if (picoEvent.detail.message === "The color sensor is not properly connected") {
+            //Show failure to calibrate
+            failedCalibration(true)
+        }
+    })
+
+    // When you click the calibrate button... calibrate (stage 1)
+    calibrateButton.addEventListener("click", () => {
+        calibrateButton.setAttribute("calibrating", "")
+        // Only if the calibration modal is open
+        if (calibrateModal.hasAttribute("open")) {
+            calibrate()
+        }
+    })
+    //When you click the outcome button (stage 2)
+    outcomeButton.addEventListener("click", () => {
+        if (outcomeButton.textContent === "Retry") { // We know we are in the failed state
 
         }
-    }, 1000)
-}
+        else { // We know we are in the success state!
+            calibrateOutcomeModal.close()
+        }
+    })
+    function calibrate(failed = false) {
+        //if failed is true then its a timeout... could be useful for a restart
+        if (failed) {
+            pico.restart()
+            pico.addEventListener("connect", ()=> {
+                pico.calibrate()
+                setTimeout(() => {
+                    if (!calibrated && !calibrateOutcomeModal?.hasAttribute("open")) failedCalibration()
+                }, 1000)
+            }, {once: true})
+        }
+        else {
+            pico.calibrate()
+            setTimeout(() => {
+                if (!calibrated && !calibrateOutcomeModal?.hasAttribute("open")) failedCalibration()    
+            }, 1000)    
+        }
+    }
+    function failedCalibration(colourSensor = false) {
+        if (!outcomeText) return
+        if (!outcomeButton) return
+        if (!outcomeTitle) return
+        if (!calibrateModal) return
+        if (!calibrateOutcomeModal) return
+        if (colourSensor) {
+            outcomeText.textContent = "Calibration was not successful!\nIt seems that the color sensor is not connected properly to the Pico\nPlease ensure that the SDA wire is connected to GPIO20 and SCL to GPIO21 (and the light is on)"
+        }
+        else {
+            outcomeText.textContent = "Calibration was not successful!\nIt seems that the Ro/Box is not responding to instructions!\nPlease follow the UF2 flashing guide to ensure a fresh and up to date Ro/Box"
+        }
+        if (outcomeButton.hasAttribute("calibrating")) outcomeButton.removeAttribute("calibrating")
+        outcomeButton.innerHTML = 'Retry<i class="fa-solid fa-spinner fa-spin"></i>'
+        outcomeButton.addEventListener("click", () => {
+            outcomeButton.setAttribute("calibrating", "")
+            calibrate(true)
+        }, {once: true})
+        if (calibrateModal.hasAttribute("open")) calibrateModal.close()
+        if (calibrateOutcomeModal.hasAttribute("open")) calibrateOutcomeModal.close()
+        outcomeTitle.textContent = "Ro/Box Failed Calibration"
+        calibrateOutcomeModal?.showModal()
+    }
+})
+
