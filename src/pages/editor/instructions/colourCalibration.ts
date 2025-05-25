4152f06aa751ac37cd1876b4c4fb894c35082307
Cowbilet <sebgtaylor@gmail.com>
2025-05-25 17:10:44 +1000

    added stage 1 colour calibration logic

--- src/pages/editor/instructions/colourCalibration.ts
+++ src/pages/editor/instructions/colourCalibration.ts
@@ -1,5 +1,31 @@
+import { pico } from "../communication/communicate"
+let calibrated = false
 document.addEventListener("DOMContentLoaded", () => {
     let calibrateModal = document.getElementById("color-calibration-modal") as HTMLDialogElement | null
     if (!calibrateModal) return
+    let calibrateOutcomeModal = document.getElementById("calibrate-outcome-button") as HTMLDialogElement | null
+    if (!calibrateOutcomeModal) return
     calibrateModal.showModal()
+    let calibrateButton = document.getElementById("calibrate-button") as HTMLButtonElement | null
+    if (!calibrateButton) return
+    calibrateButton.addEventListener("click", calibrate)
+
+    pico.addEventListener("calibrated", () => {
+        calibrated = true //Successful calibration!
+        calibrateModal.close()
+        let outcomeText = calibrateOutcomeModal.querySelector(".modal-content p") as HTMLParagraphElement | null
+        if (!outcomeText) return
+        let outcomeButton = calibrateOutcomeModal.querySelector("#calibrate-outcome-button") as HTMLButtonElement | null
+        if (!outcomeButton) return
+        outcomeText.textContent = "Calibration was a success!\nThe calibration settings have been saved to the Ro/Box's Pico."
+        outcomeButton.textContent = "Done"
+    })
 })
+function calibrate() {
+    pico.calibrate()
+    setTimeout(() => {
+        if (!calibrated) { // Failed for calibrate
+
+        }
+    }, 1000)
+}
