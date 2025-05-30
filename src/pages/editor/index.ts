ad1d7391cf20ccc8fee1f3b3d2f000edf613afec
Cowbilet <sebgtaylor@gmail.com>
2025-05-30 16:25:05 +1000

    added more context to ashtons controls

--- src/pages/editor/index.ts
+++ src/pages/editor/index.ts
@@ -72,7 +72,9 @@ document.addEventListener("DOMContentLoaded", () => {
     }
     else return
     if (!project) return
-    //Register Ashtons controls
+// Control + scroll for zoom,
+// Scroll for vertical movement,
+// Shift + scroll for horizontal movement
     registerControls(workspace)
     if (navigator.serial) {
         postBlocklyWSInjection()
