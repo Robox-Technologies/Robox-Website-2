12c0f707e19d4311be11edca440384c0c14899d4
Yuma Soerianto <hi@madebyyuma.com>
2025-07-01 19:48:56 +1000

    Handle stripe errors/no secret in confirmation

--- src/pages/shop/checkout/confirmation/confirmation.ts
+++ src/pages/shop/checkout/confirmation/confirmation.ts
@@ -4,29 +4,47 @@ import { loadStripe } from '@stripe/stripe-js';
 const urlParams = new URLSearchParams(window.location.search);
 const paymentIntentClientSecret = urlParams.get("payment_intent_client_secret");
 
-const stripe = await loadStripe(stripePublishableKey);
+if (paymentIntentClientSecret) {
+    try {
+        const stripe = await loadStripe(stripePublishableKey);
+        
+        let result = "retry";
+        while (result === "retry") {
+            result = await pollIntent(stripe);
+        }
+    } catch (error) {
+        console.warn("loadStripe/pollIntent failed with error: ", error);
+        showFailure();
+    }
+} else {
+    console.warn("No payment intent client secret found in URL params");
+    showFailure();
+}
 
-async function pollIntent(): Promise<string> {
+function showSuccess(email: string) {
+    document.getElementById("loading").style.display = "none";
+    document.getElementById("failure").style.display = "none";
+    document.getElementById("success").style.display = "block";
+    document.getElementById("email").textContent = email;
+    wipeCart();
+}
+
+function showFailure() {
+    document.getElementById("loading").style.display = "none";
+    document.getElementById("failure").style.display = "block";
+    document.getElementById("success").style.display = "none";
+}
+
+async function pollIntent(stripe: Stripe): Promise<string> {
     let paymentIntentResult = await stripe.retrievePaymentIntent(paymentIntentClientSecret);
     let paymentIntent = paymentIntentResult.paymentIntent;
 
     if (!paymentIntent || paymentIntent.status === "processing") return "retry";
     
-    document.getElementById("loading").style.display = "none";
 
     if (paymentIntent.status === "succeeded") {
-        document.getElementById("failure").style.display = "none";
-        document.getElementById("success").style.display = "block";
-        document.getElementById("email").textContent = paymentIntent.receipt_email;
-        wipeCart();
+        showSuccess(paymentIntent.receipt_email);
     } else {
-        document.getElementById("failure").style.display = "block";
-        document.getElementById("success").style.display = "none";
+        showFailure();
     }
-}
-
-let result = "retry";
-
-while (result === "retry") {
-    result = await pollIntent();
 }
